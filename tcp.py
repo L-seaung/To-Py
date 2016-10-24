077628e2b15b2d8206def3aa74e10241f34a9891
L-seaung <2968055492@qq.com>
2016-10-24 15:24:51 +0800

    python programming
    tcp script update

--- tcp.py
+++ tcp.py
@@ -5,10 +5,27 @@ import socket
 BUFF = 1024
 HOST = ''
 PORT = 65534
+ADDRESS = (HOST, PORT)
 
 sock = socket.socket(socket.AF_INET, socket.STREAM)
 
 def tcpServer(host, port):
     try:
-        sock.connect((HOST, PORT))
+        print "please wating connection......"
+        sock.connect(ADDRESS)
+    except Exception as e:
+        print " %s erros!" % e
+    try:
+        data_recv = sock.recv(BUFF)
+        print "recvfrom %s data..." % data_recv
+    except Exception as e:
+        print "%s errors!" % e
         
+def main():
+    pass
+        
+        
+        
+        
+if __name__ == '__main__':
+    main()
