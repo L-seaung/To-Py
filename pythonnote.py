93d19b7d2a36288c5f0984220aa7bed707374641
L-seaung <2968055492@qq.com>
2016-10-27 19:33:33 +0800

    about python
    code

---
+++ pythonnote.py
@@ -0,0 +1,22 @@
+0x1 python indentation
+
+Python uses code indentation to represent hierarchies
+
+例如：
+    if a > b:
+        print(a)
+    else:
+        print(b)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
